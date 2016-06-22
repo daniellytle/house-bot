69825642568d2c5906b96b386fabc90e4b615a70
Dan Wilson <wilsoda@umich.edu>
2016-06-21 19:48:59 -0700

    gen

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook/', function (req, res) {
 				sendGenericMessage(sender)
 				continue
 			}
-			sendTextMessage(sender, "Ay girl " + event.message)
+			sendTextMessage(sender, "Ay girl " + event.message.stringify())
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
