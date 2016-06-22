85fba961c1558118e1cb9f4e31a8fb18a6dd82ea
Dan Wilson <wilsoda@umich.edu>
2016-06-21 19:17:45 -0700

    yup

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post('/webhook/', function (req, res) {
 })
 
 
-const token = "<PAGE_ACCESS_TOKEN>"
+const token = "EAAYJEqkK414BALvkePuTID6Ln3pWxIj3G3eNsC1X2wMgIx30R6pW9GR1qhKqapiwDBGdOqZAqKpFGoZCAFsbsMnwd0BgxeZBFtXTrAbiDQOL6zC2gHMZBgoNNxmHVuLOB6cALDx6AVu0z8UVnNk3ADtF2NBgYPfq2Dc7uiuLmAZDZD"
 
 function sendTextMessage(sender, text) {
 	let messageData = { text:text }
