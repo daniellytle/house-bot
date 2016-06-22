9cbdc8cc06e33dfca1ce66314853240216a93aa3
Dan Wilson <wilsoda@umich.edu>
2016-06-21 20:57:03 -0700

    gen

--- manager.js
+++ manager.js
@@ -1,16 +1,18 @@
-// let messenger = require('./messenger.js');
+let messenger = require('./messenger.js');
 var giphy = require('./modules/giphy.js');
 // let quotes = require('./modules/quotes.js');
 
 module.exports = {
-	handle: function(sender, command, msg, callback) {
+	handle: function(sender, command, msg) {
 
 		console.log("command:", command, msg);
 		var text = "";
 
 		if (command === "giphy") {
 			console.log('getting giph')
-			giphy.getGif(msg, callback);
+			giphy.getGif(msg, function(url) {
+				messenger.sendImage(sender, url);
+			});
 		} else {
 			callback(msg[0]);
 		}
--- messenger.js
+++ messenger.js
@@ -1,10 +1,40 @@
+var request = require('request');
 module.exports = {
 	
 	/* Sends Messages */
 
-	function send(sender, text) {
+	sendTextMessage: function(sender, text) {
 		let messageData = { text:text }
 		
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: {access_token:token},
+			method: 'POST',
+			json: {
+				recipient: {id:sender},
+				message: messageData,
+			}
+		}, function(error, response, body) {
+			if (error) {
+				console.log('Error sending messages: ', error)
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error)
+			}
+		})
+	},
+
+	sendImage: function(sender, url) {
+		let messageData = {
+			"attachment": {
+				"type": "template",
+				"payload": {
+					"template_type": "generic",
+					"elements": [{				
+						"image_url": url					
+					}]
+				}
+			}
+		}
 		request({
 			url: 'https://graph.facebook.com/v2.6/me/messages',
 			qs: {access_token:token},
