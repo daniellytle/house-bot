2920fb3787439294ea04c5f0b7693ea6f1b95f1a
Dan Wilson <wilsoda@umich.edu>
2016-06-21 21:49:53 -0700

    auto

--- manager.js
+++ manager.js
@@ -5,14 +5,13 @@ var convers = require('./modules/conversation.js');
 module.exports = {
 	handle: function(sender, command, msg, text) {
 
-		console.log("command:", command);
-
 		if (command === "giphy") {
-			console.log('getting giph')
+			// Giphy Gif
 			giphy.getGif(msg, function(url) {
 				messenger.sendImage(sender, url);
 			});
 		} else {
+			// AI response
 			convers.getRes(text, function(err, response, data) {
 				// Shorten
 				var text = data.result.fulfillment.speech.substring(0, 200);
